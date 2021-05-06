6b6ac47553cef78c94ceb22a58628b742c6d894f
nikodallanoce <dllnik97@gmail.com>
2021-05-07 00:35:25 +0200

    heroku fixed shh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,6 @@ public class Main {
         final String username = System.getenv("mqttUsr");
         final String password = System.getenv("mqttPsw");
 
-        try {
-            System.out.println(System.getenv("tokenBot"));
-            System.out.println(System.getenv("mongoConnection"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         MQTTServer server = new MQTTServer("server", host, new MongoConnection("Mqttempv2"));
         server.connect(username, password);
         server.subscribe("#");
